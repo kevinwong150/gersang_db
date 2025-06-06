ba54f5bd0e70f0042cf3f018b5ef17bcd71c3a11
kevinwong150 <kevinwyl@yahoo.com.hk>
2025-06-07 05:46:22 +0800

    make sample recipes for testing

--- apps/gersang_db/priv/repo/seeds.exs
+++ apps/gersang_db/priv/repo/seeds.exs
@@ -12,8 +12,12 @@
 
 gersang_item_attrs = [
   %{
-    name: "Green Force Stone",
-    market_price: 0
+    name: "Sealed Power Piece",
+    market_price: 1600000
+  },
+  %{
+    name: "Sealed Power Shard",
+    market_price: 17000000
   },
   %{
     name: "Yellow Force Stone",
@@ -34,6 +38,30 @@ gersang_item_attrs = [
   %{
     name: "Sun Seal",
     market_price: 1000
+  },
+  %{
+    name: "Five color powder",
+    market_price: 1500000
+  },
+  %{
+    name: "Force of Deities(Thunder)",
+    market_price: 35000000
+  },
+  %{
+    name: "Force of Deities(Earth)",
+    market_price: 35000000
+  },
+  %{
+    name: "Force of Deities(Wind)",
+    market_price: 35000000
+  },
+  %{
+    name: "Force of Deities(Water)",
+    market_price: 35000000
+  },
+  %{
+    name: "Force of Deities(Flame)",
+    market_price: 35000000
   },
   %{
     name: "Piece of Deities(Thunder)",
@@ -74,9 +102,85 @@ gersang_item_attrs = [
   %{
     name: "Small Elemental Stone of Earth",
     market_price: 8_500_000
-  }
+  },
+  %{
+    name: "Force Beads(Flame)",
+    market_price: 0
+  },
+  %{
+    name: "Force Beads(Water)",
+    market_price: 0
+  },
+  %{
+    name: "Force Beads(Wind)",
+    market_price: 0
+  },
+  %{
+    name: "Force Beads(Thunder)",
+    market_price: 0
+  },
+  %{
+    name: "Force Beads(Earth)",
+    market_price: 0
+  },
+  %{
+    name: "Fragmented Force Beads(Flame)",
+    market_price: 2900000
+  },
+  %{
+    name: "Fragmented Force Beads(Water)",
+    market_price: 2_800_000
+  },
+  %{
+    name: "Fragmented Force Beads(Wind)",
+    market_price: 275_000
+  },
+  %{
+    name: "Fragmented Force Beads(Thunder)",
+    market_price: 1200000
+  },
+  %{
+    name: "Fragmented Force Beads(Earth)",
+    market_price: 500000
+  },
+  %{
+    name: "Flame Stone",
+    market_price: 1000000
+  },
+  %{
+    name: "Frozen Stone",
+    market_price: 1000000
+  },
+  %{
+    name: "maple stone",
+    market_price: 1000000
+  },
+  %{
+    name: "Thunder Soul Stone",
+    market_price: 1000000
+  },
+  %{
+    name: "Earth specter stone",
+    market_price: 500000
+  },
+  %{
+    name: "Flame specter stone",
+    market_price: 4000000
+  },
+  %{
+    name: "Water specter stone",
+    market_price: 2000000
+  },
+  %{
+    name: "Wind specter stone",
+    market_price: 2000000
+  },
+  %{
+    name: "Thunder specter stone",
+    market_price: 2000000
+  },
 ]
 
 Enum.map(gersang_item_attrs, fn attr ->
-  GersangDb.GersangItem.create_item(attr)
+  # GersangDb.GersangItem.create_item(attr)
 end)
--- apps/gersang_db/priv/repo/structure.sql
+++ apps/gersang_db/priv/repo/structure.sql
@@ -141,6 +141,48 @@ ALTER TABLE ONLY public.recipes ALTER COLUMN id SET DEFAULT nextval('public.reci
 --
 
 COPY public.gersang_items (id, name, tags, margin, market_price, cost_per, "artisan_product?", artisan_production_amount, artisan_production_fee, inserted_at, updated_at) FROM stdin;
+54	Sealed Power Piece	\N	\N	1600000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+55	Sealed Power Shard	\N	\N	17000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+56	Yellow Force Stone	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+57	Red Force Stone	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+58	Blue Force Stone	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+59	Moon Seal	\N	\N	2000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+60	Sun Seal	\N	\N	1000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+61	Five color powder	\N	\N	1500000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+62	Force of Deities(Thunder)	\N	\N	35000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+63	Force of Deities(Earth)	\N	\N	35000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+64	Force of Deities(Wind)	\N	\N	35000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+65	Force of Deities(Water)	\N	\N	35000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+66	Force of Deities(Flame)	\N	\N	35000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+67	Piece of Deities(Thunder)	\N	\N	850000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+68	Piece of Deities(Earth)	\N	\N	890000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+69	Piece of Deities(Wind)	\N	\N	1400000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+70	Piece of Deities(Water)	\N	\N	1980000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+71	Piece of Deities(Flame)	\N	\N	3000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+72	Small Elemental Stone of Flame	\N	\N	940000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+73	Small Elemental Stone of Water	\N	\N	850000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+74	Small Elemental Stone of Wind	\N	\N	790000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+75	Small Elemental Stone of Thunder	\N	\N	850000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+76	Small Elemental Stone of Earth	\N	\N	8500000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+77	Force Beads(Flame)	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+78	Force Beads(Water)	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+79	Force Beads(Wind)	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+80	Force Beads(Thunder)	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+81	Force Beads(Earth)	\N	\N	0	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+82	Fragmented Force Beads(Flame)	\N	\N	2900000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+83	Fragmented Force Beads(Water)	\N	\N	2800000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+84	Fragmented Force Beads(Wind)	\N	\N	275000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+85	Fragmented Force Beads(Thunder)	\N	\N	1200000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+86	Fragmented Force Beads(Earth)	\N	\N	500000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+87	Flame Stone	\N	\N	1000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+88	Frozen Stone	\N	\N	1000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+89	maple stone	\N	\N	1000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+90	Thunder Soul Stone	\N	\N	1000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+91	Earth specter stone	\N	\N	500000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+92	Flame specter stone	\N	\N	4000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+93	Water specter stone	\N	\N	2000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+94	Wind specter stone	\N	\N	2000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
+95	Thunder specter stone	\N	\N	2000000	\N	f	\N	\N	2025-06-06 21:30:38	2025-06-06 21:30:38
 \.
 
 
@@ -149,6 +191,18 @@ COPY public.gersang_items (id, name, tags, margin, market_price, cost_per, "arti
 --
 
 COPY public.recipes (id, product_item_id, material_item_id, media, material_amount, inserted_at, updated_at) FROM stdin;
+1	55	54	Masamune	10	2025-06-06 21:36:23	2025-06-06 21:36:23
+6	83	83	Zhang Guolao	10	2025-06-06 21:38:23	2025-06-06 21:38:23
+7	83	73	Zhang Guolao	5	2025-06-06 21:38:23	2025-06-06 21:38:23
+8	83	88	Zhang Guolao	2	2025-06-06 21:38:23	2025-06-06 21:38:23
+9	83	61	Zhang Guolao	2	2025-06-06 21:38:23	2025-06-06 21:38:23
+10	77	82	Zhang Guolao	10	2025-06-06 21:39:02	2025-06-06 21:39:02
+11	77	72	Zhang Guolao	5	2025-06-06 21:39:02	2025-06-06 21:39:02
+12	77	87	Zhang Guolao	2	2025-06-06 21:39:02	2025-06-06 21:39:02
+13	77	61	Zhang Guolao	2	2025-06-06 21:39:02	2025-06-06 21:39:02
+14	72	59	test	5	2025-06-06 21:43:49	2025-06-06 21:43:49
+15	72	60	test	5	2025-06-06 21:43:49	2025-06-06 21:43:49
+16	61	54	test	5	2025-06-06 21:44:36	2025-06-06 21:44:36
 \.
 
 
@@ -166,14 +220,14 @@ COPY public.schema_migrations (version, inserted_at) FROM stdin;
 -- Name: gersang_items_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gersang_db
 --
 
-SELECT pg_catalog.setval('public.gersang_items_id_seq', 53, true);
+SELECT pg_catalog.setval('public.gersang_items_id_seq', 95, true);
 
 
 --
 -- Name: recipes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: gersang_db
 --
 
-SELECT pg_catalog.setval('public.recipes_id_seq', 1, false);
+SELECT pg_catalog.setval('public.recipes_id_seq', 16, true);
 
 
 --
